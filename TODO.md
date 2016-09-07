6238866e240ba98fa88c81af6b442bb979f0a77a
Tony Brock <abrock17@gmail.com>
2016-09-06 21:27:39 -0400

    much refactoring... still a mess, but tests are green

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 handle invalid characters
 handle invalid roman numerals (such as IIII or VV) ?
 handle addend or sum too big
+malloc size
--- roman-numeral-calculator.c
+++ roman-numeral-calculator.c
@@ -3,13 +3,15 @@
 
 #include "roman-numeral-calculator.h"
 
-struct Wire {
+#define abacus_rows 7
+
+struct Row {
 	char character;
 	int count;
 };
 
 struct Abacus {
-	struct Wire wires[7];
+	struct Row rows[abacus_rows];
 	int m, d, c, l, x, v, i;
 };
 
@@ -30,160 +32,228 @@ struct Abacus initialize_abacus(void) {
 	return abacus;
 }
 
-struct Wire* get_wire(char character, struct Abacus* abacus) {
+int get_row_index(char character, struct Abacus* abacus) {
 	int index;
 
-	for (index = 0; index < sizeof(abacus->wires); index++) {
-		if (character == abacus->wires[index].character) {
-			return &abacus->wires[index];
+	for (index = 0; index < abacus_rows; index++) {
+		if (character == abacus->rows[index].character) {
+			return index;
 		}
 	}
 
-	return NULL;
+	return -1;
 }
 
 void tally_numerals(char* numerals, struct Abacus* abacus) {
 	int end;
 
 	end = strlen(numerals) - 1;
-	char current = NULL;
 	char previous = NULL;
 	int index;
+	int row_index;
 	for (index = end; index >= 0; index--) {
-		current = numerals[index];
-		if (current == 'I') {
-			if (previous == 'X') {
-				abacus->x--;
-				abacus->i += 9;
-			} else if (previous == 'V') {
-				abacus->v--;
-				abacus->i += 4;
-			} else {
-				abacus->i++;
-			}
-		} else if (current == 'V') {
-			abacus->v++;
-		} else if (current == 'X') {
-			if (previous == 'C') {
-				abacus->c--;
-				abacus->x += 9;
-			} else if (previous == 'L') {
-				abacus->l--;
-				abacus->x += 4;
-			} else {
-				abacus->x++;
-			}
-		} else if (current == 'L') {
-			abacus->l++;
-		} else if (current == 'C') {
-			if (previous == 'M') {
-				abacus->m--;
-				abacus->c += 9;
-			} else if (previous == 'D') {
-				abacus->d--;
-				abacus->c += 4;
-			} else {
-				abacus->c++;
-			}
-		} else if (current == 'D') {
-			abacus->d++;
-		} else if (current == 'M') {
-			abacus->m++;
-		}
+		row_index = get_row_index(numerals[index], abacus);
 
-		previous = current;
+		if (row_index == (abacus_rows - 1) || row_index % 2 > 0) {
+
+			abacus->rows[row_index].count++;
+
+		} else {
+
+			if (previous == abacus->rows[row_index + 1].character) {
+
+				abacus->rows[row_index + 1].count--;
+				abacus->rows[row_index].count += 4;
+
+			} else if (previous == abacus->rows[row_index + 2].character) {
+
+				abacus->rows[row_index + 2].count--;
+				abacus->rows[row_index].count += 9;
+
+			} else {
+				abacus->rows[row_index].count++;
+			}
+		}
+		// if (current == 'I') {
+		// 	if (previous == 'X') {
+		// 		abacus->x--;
+		// 		abacus->i += 9;
+		// 	} else if (previous == 'V') {
+		// 		abacus->v--;
+		// 		abacus->i += 4;
+		// 	} else {
+		// 		abacus->i++;
+		// 	}
+		// } else if (current == 'V') {
+		// 	abacus->v++;
+		// } else if (current == 'X') {
+		// 	if (previous == 'C') {
+		// 		abacus->c--;
+		// 		abacus->x += 9;
+		// 	} else if (previous == 'L') {
+		// 		abacus->l--;
+		// 		abacus->x += 4;
+		// 	} else {
+		// 		abacus->x++;
+		// 	}
+		// } else if (current == 'L') {
+		// 	abacus->l++;
+		// } else if (current == 'C') {
+		// 	if (previous == 'M') {
+		// 		abacus->m--;
+		// 		abacus->c += 9;
+		// 	} else if (previous == 'D') {
+		// 		abacus->d--;
+		// 		abacus->c += 4;
+		// 	} else {
+		// 		abacus->c++;
+		// 	}
+		// } else if (current == 'D') {
+		// 	abacus->d++;
+		// } else if (current == 'M') {
+		// 	abacus->m++;
+		// }
+
+		previous = numerals[index];
 	}
 }
 
-void set_char_and_increment(char* string, char character, int* index) {
+void reduce(struct Abacus* abacus) {
+	int index;
+	for (index = 0; index < abacus_rows - 1; index++) {
+		if (index % 2 == 0) {
+			while (abacus->rows[index].count >= 5) {
+				abacus->rows[index + 1].count++;
+				abacus->rows[index].count -= 5;
+			}
+		} else {
+			if (abacus->rows[index].count >= 2) {
+				abacus->rows[index + 1].count++;
+				abacus->rows[index].count -= 2;
+			}
+		}
+	}
+}
+
+void append(char* string, char character, int* index) {
 	// printf("string : %s, character : %c, index : %d\n", string, character, *index);
 	string[*index] = character;
 	(*index)++;
 }
 
-void set_char_and_increment_n_times(char* string, char character, int* index, int n) {
+void append_n_times(char* string, char character, int* index, int n) {
 	int count;
 	for (count = 0; count < n; count++) {
-		set_char_and_increment(string, character, index);
+		append(string, character, index);
 	}	
 }
 
 char* add(char* addend1, char* addend2) {
 	struct Abacus abacus = initialize_abacus();
 
+	printf("addend1 : %s, addend2 : %s\n", addend1, addend2);
+
 	tally_numerals(addend1, &abacus);
 	tally_numerals(addend2, &abacus);
 
-	while (abacus.i >= 5) {
-		abacus.v++;
-		abacus.i -= 5;
-	}
+	printf("abacus :  i : %d, v : %d, x : %d\n", abacus.rows[0].count, abacus.rows[1].count, abacus.rows[2].count);
 
-	if (abacus.v >= 2) {
-		abacus.x++;
-		abacus.v -= 2;
-	}
+	reduce(&abacus);
 
-	while (abacus.x >= 5) {
-		abacus.l++;
-		abacus.x -= 5;
-	}
+	printf("abacus :  i : %d, v : %d, x : %d\n", abacus.rows[0].count, abacus.rows[1].count, abacus.rows[2].count);
 
-	if (abacus.l >= 2) {
-		abacus.c++;
-		abacus.l -= 2;
-	}
+	// while (abacus.i >= 5) {
+	// 	abacus.v++;
+	// 	abacus.i -= 5;
+	// }
 
-	while (abacus.c >= 5) {
-		abacus.d++;
-		abacus.c -= 5;
-	}
+	// if (abacus.v >= 2) {
+	// 	abacus.x++;
+	// 	abacus.v -= 2;
+	// }
 
-	if (abacus.d >= 2) {
-		abacus.m++;
-		abacus.d -= 2;
-	}
+	// while (abacus.x >= 5) {
+	// 	abacus.l++;
+	// 	abacus.x -= 5;
+	// }
+
+	// if (abacus.l >= 2) {
+	// 	abacus.c++;
+	// 	abacus.l -= 2;
+	// }
+
+	// while (abacus.c >= 5) {
+	// 	abacus.d++;
+	// 	abacus.c -= 5;
+	// }
+
+	// if (abacus.d >= 2) {
+	// 	abacus.m++;
+	// 	abacus.d -= 2;
+	// }
 
 	char* sum = malloc(abacus.c + abacus.l + abacus.x + abacus.v + abacus.i + 1);
 	int index = 0;
+	int row_index;
 
-	set_char_and_increment_n_times(sum, 'M', &index, abacus.m);
+	for (row_index = abacus_rows - 1; row_index >= 0; row_index--) {
+		if (row_index % 2 != 0) {
+			if (abacus.rows[row_index - 1].count == 4) {
+				append(sum, abacus.rows[row_index - 1].character, &index);
 
-	if (abacus.c == 4) {
-		set_char_and_increment(sum, 'C', &index);
-		if (abacus.d > 0) {
-			set_char_and_increment(sum, 'M', &index);
+				if (abacus.rows[row_index].count == 0) {
+					append(sum, abacus.rows[row_index].character, &index);
+				} else {
+					append(sum, abacus.rows[row_index + 1].character, &index);
+				}
+
+				row_index--;
+			} else {
+				append_n_times(sum, abacus.rows[row_index].character, &index, abacus.rows[row_index].count);
+			}
 		} else {
-			set_char_and_increment(sum, 'D', &index);
+			append_n_times(sum, abacus.rows[row_index].character, &index, abacus.rows[row_index].count);
 		}
-	} else {
-		set_char_and_increment_n_times(sum, 'D', &index, abacus.d);
-		set_char_and_increment_n_times(sum, 'C', &index, abacus.c);
+		printf("sum : %s\n", sum);
 	}
 
-	if (abacus.x == 4) {
-		set_char_and_increment(sum, 'X', &index);
-		if (abacus.l > 0) {
-			set_char_and_increment(sum, 'C', &index);
-		} else {
-			set_char_and_increment(sum, 'L', &index);
-		}
-	} else {
-		set_char_and_increment_n_times(sum, 'L', &index, abacus.l);
-		set_char_and_increment_n_times(sum, 'X', &index, abacus.x);
-	}
+	// append_n_times(sum, 'M', &index, abacus.m);
 
-	if (abacus.i == 4) {
-		set_char_and_increment(sum, 'I', &index);
-		if (abacus.v > 0) {
-			set_char_and_increment(sum, 'X', &index);
-		} else {
-			set_char_and_increment(sum, 'V', &index);
-		}
-	} else {
-		set_char_and_increment_n_times(sum, 'V', &index, abacus.v);
-		set_char_and_increment_n_times(sum, 'I', &index, abacus.i);
-	}
+	// if (abacus.c == 4) {
+	// 	append(sum, 'C', &index);
+	// 	if (abacus.d > 0) {
+	// 		append(sum, 'M', &index);
+	// 	} else {
+	// 		append(sum, 'D', &index);
+	// 	}
+	// } else {
+	// 	append_n_times(sum, 'D', &index, abacus.d);
+	// 	append_n_times(sum, 'C', &index, abacus.c);
+	// }
+
+	// if (abacus.x == 4) {
+	// 	append(sum, 'X', &index);
+	// 	if (abacus.l > 0) {
+	// 		append(sum, 'C', &index);
+	// 	} else {
+	// 		append(sum, 'L', &index);
+	// 	}
+	// } else {
+	// 	append_n_times(sum, 'L', &index, abacus.l);
+	// 	append_n_times(sum, 'X', &index, abacus.x);
+	// }
+
+	// if (abacus.i == 4) {
+	// 	append(sum, 'I', &index);
+	// 	if (abacus.v > 0) {
+	// 		append(sum, 'X', &index);
+	// 	} else {
+	// 		append(sum, 'V', &index);
+	// 	}
+	// } else {
+	// 	append_n_times(sum, 'V', &index, abacus.v);
+	// 	append_n_times(sum, 'I', &index, abacus.i);
+	// }
 
 	sum[index] = '\0';
 
